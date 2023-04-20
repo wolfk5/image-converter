d71f33ed5c7a1779d75ac11e19adfe285aa0a52c
wolfk5 <130686276+wolfk5@users.noreply.github.com>
2023-04-20 09:42:28 -0700

    Add files via upload

---
+++ Python image converter/ImageConvert.py
@@ -0,0 +1,17 @@
+from PIL import Image
+choose = int(input('put number to choose which image to show'))
+
+if (choose == 1):
+    image1 = Image.open('khuaaoxanh.jpg')
+    image1.show()
+    while (choose == 1):
+        converttype = int(input('put number to choose type of converter'))
+        if (converttype == 1):
+            
+ 
+ 
+            
+            
+if (choose ==2):
+    image2 = Image.open('platypus.jpg')
+    image2.show()
